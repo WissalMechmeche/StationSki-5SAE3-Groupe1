4a380c908618210f77adfdab5b7d3bcb5dfe6cd5
WissalMechmeche <mechmechwissal@gmail.com>
2024-10-29 18:03:11 +0100

    Sonarqubee

--- src/main/java/tn/esprit/spring/entities/Course.java
+++ src/main/java/tn/esprit/spring/entities/Course.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 import java.util.Set;
 
 import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
@@ -21,21 +25,35 @@ public class Course implements Serializable {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	Long numCourse;
 
+	@NotNull
+	@Min(1)
+	@Max(10)
 	int level;
+
 	@Enumerated(EnumType.STRING)
 	TypeCourse typeCourse;
+
 	@Enumerated(EnumType.STRING)
 	Support support;
+
+	@NotNull
+	@Min(0)
 	Float price;
+
 	int timeSlot;
 
-	// New attributs
+
+	@NotEmpty
 	String description;
+
+	@NotEmpty
 	String location;
 
+
 	@JsonIgnore
-	@OneToMany(mappedBy= "course")
+	@OneToMany(mappedBy = "course", fetch = FetchType.LAZY)
 	private Set<Registration> registrations;
+
 
 
 

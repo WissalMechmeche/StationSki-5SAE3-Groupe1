a7920ccba66441eb2aac8edc2a0b12af8bd22096
walid.elhadhri <walid.elhadhri@gmail.com>
2024-11-07 13:26:28 +0100

    model

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -1,9 +1,16 @@
 package tn.esprit.spring;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
+
 class GestionStationSkiApplicationTests {
 
 	@Test
